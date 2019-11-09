48c9c11ade51a5237a3f9e125d09825a96d52061
Prajwol Lamichhane <prajwollamichhane11@gmail.com>
2019-11-09 19:52:56 +0545

    Problem 5

---
+++ Introductions/matchingWordAndNonWordCharacter.py
@@ -0,0 +1,5 @@
+# The expression \w will match any word character.
+# Word characters include alphanumeric characters (-, - and -) and underscores (_).
+# \W matches any non-word character.
+# Non-word characters include characters other than alphanumeric characters (-, - and -) and underscore (_).
+Regex_Pattern = r"\w\w\w\W\w\w\w\w\w\w\w\w\w\w\W\w\w\w"	# Do not delete 'r'.
